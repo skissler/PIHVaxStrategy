017810a9b26addc6dc4ebe32df6863b947451d96
Stephen Kissler <skissler@hsph.harvard.edu>
2021-02-20 00:00:55 -0500

    Got a first pass at vaccination running

--- code/sim_epi.R
+++ code/sim_epi.R
@@ -19,6 +19,19 @@ Imean <- 4 			# Mean infectious period (days)
 
 tmax <- 90			# Max days to run the simulation
 
+# A list that contains the start time for vaccination and the percent of the population at which to switch strategies. Possible strategies are "risk" (prioritize those at highest risk), "contact" (prioritize those with the most contacts), or "anyone" (vaccinate at random). Also sets the amountt of infecttion blocking and transmission blocking that the vaccine gives. I think this makes it a 'leaky' vaccine. 
+
+vax_strategy <- list(
+	tstart=30,
+	daily_pvax=0.002,
+	efficacy=0.95,
+	infblock=0,
+	transblock=0,
+	pswitch=tibble(
+		pstart=c(0,0.1), 
+		pend=c(0.1,0.6), 
+		prioritize=c("risk","contact")))
+
 # =============================================================================
 # Calculate key quantities
 # =============================================================================
@@ -41,7 +54,8 @@ casecounts <- tibble(t=1:tmax,
 	E=rep(0,tmax),	# Exposed
 	I=rep(0,tmax),	# Infectious
 	R=rep(0,tmax),	# Recovered
-	X=rep(0,tmax))	# Deceased
+	X=rep(0,tmax),	# Deceased
+	V=rep(0,tmax))	# Vaccinated
 
 # Initialize compartment vectors:
 Evec <- matrix(rep(0,N),ncol=1)
@@ -49,12 +63,14 @@ Ivec <- matrix(rep(0,N),ncol=1)
 Ivec[1] <- 1
 Rvec <- matrix(rep(0,N),ncol=1)
 Xvec <- matrix(rep(0,N),ncol=1)
+Vvec <- matrix(rep(0,N),ncol=1)
 
 # Fill output tibble with initial compartment sums:
 casecounts$E[1] <- sum(Evec)
 casecounts$I[1] <- sum(Ivec)
 casecounts$R[1] <- sum(Rvec)
 casecounts$X[1] <- sum(Xvec)
+casecounts$V[1] <- sum(Vvec)
 
 # Increment time:
 t <- 2
@@ -67,21 +83,28 @@ while(t<=tmax & (sum(Evec)+sum(Ivec))>0){
 	# Define adjacency matrix here if it's meant to be dynamic:
 	# A <- make_adj(weights=Wi) 
 
-	newE <- (runif(N)<(pinf*A%*%Ivec))*(1-Evec)*(1-Ivec)*(1-Rvec)
+	newE <- (runif(N)<(pinf*A%*%Ivec))*(1-Evec)*(1-Ivec)*(1-Rvec)*(1-Xvec)
 	newI <- (runif(N)<(1/Emean*Evec))*1
 	newR <- (runif(N)<(1/Imean*Ivec))*1
-	newX <- (runif(N)<(newR*mortrisk))*1
+	newX <- (runif(N)<(newR*mortrisk*(1-Vvec*vax_strategy$efficacy)))*1
 	newR <- newR - newX
+	if(t>=(vax_strategy$tstart)){
+		newV <- (runif(N)<((vax_strategy$daily_pvax)*(1-Xvec)*(1-Vvec)))
+	} else {
+		newV <- matrix(rep(0,N),ncol=1)
+	}
 
 	Evec <- Evec + newE - newI
 	Ivec <- Ivec + newI - newR
 	Rvec <- Rvec + newR
 	Xvec <- Xvec + newX
+	Vvec <- Vvec + newV
 
 	casecounts$E[t] <- sum(Evec)
 	casecounts$I[t] <- sum(Ivec)
 	casecounts$R[t] <- sum(Rvec)
 	casecounts$X[t] <- sum(Xvec)
+	casecounts$V[t] <- sum(Vvec)
 
 	t <- t+1
 
@@ -90,7 +113,7 @@ while(t<=tmax & (sum(Evec)+sum(Ivec))>0){
 casecounts[(t-1):tmax,-1] <- casecounts[t-1,-1]
 
 fig_casecounts <- casecounts %>% 
-	pivot_longer(c("E","I","R","X")) %>%
+	pivot_longer(c("E","I","R","X","V")) %>%
 	ggplot(aes(x=t, y=value, col=name)) + 
 		geom_point(size=0.5) + 
 		geom_line() + 
@@ -100,16 +123,6 @@ fig_casecounts <- casecounts %>%
 
 
 
-# A list that contains the start time for vaccination and the percent of the population at which to switch strategies. Possible strategies are "risk" (prioritize those at highest risk), "contact" (prioritize those with the most contacts), or "anyone" (vaccinate at random). Also sets the amountt of infecttion blocking and transmission blocking that the vaccine gives. I think this makes it a 'leaky' vaccine. 
-
-vax_strategy <- list(
-	tstart=45,
-	infblock=0.5,
-	transblock=0.5,
-	pswitch=tibble(
-		pstart=c(0,0.1), 
-		pend=c(0.1,0.6), 
-		prioritize=c("risk","contact")))
 
 
 
